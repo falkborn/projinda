c47855a5cee3c3ea39f208d972e9cad6cac150c4
Jacob Schweizer <jacobschweizer@n162-p227.eduroam.kth.se>
2022-05-03 17:31:21 +0200

    second

--- cryptotracker/src/App.js
+++ cryptotracker/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'; 
 import { Layout, Typography, Space } from 'antd'; 
+import { navbar } from './components'; 
+
 const App = () => {
   return (
     <div className='app'>
         <div className='navbar'>
-
+            <navbar/>
         </div>
         <div className='main'>
 
