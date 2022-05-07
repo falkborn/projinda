79ec21d70d38b19fb455d69d4732bb23008f3b9d
Leo Falkborn <leofalkborn@Leos-MacBook-Pro.local>
2022-05-07 19:19:34 +0200

    mer kod+ny css app

--- cryptotracker/src/App.js
+++ cryptotracker/src/App.js
@@ -1,32 +1,43 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'; 
-import { Layout, Typography, Space } from 'antd'; 
-import { Navbar, Homepage, Cryptocurrencies, CryptoDetails, StockRecommendations } from './components'; 
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Homepage, Cryptocurrencies, CryptoDetails, StockRecommendations } from './components';
+import './style.css'
 
 const App = () => {
   return (
     <div className='App'>
-        <div className='Navbar'>
-            <Navbar/>
-        </div>
-        <div className='Main'>
-          <Layout>
-            <div className= "routes">
-              <Routes>
-                <Route exact path='/' element={<Homepage/>}/>
-                <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
-                <Route exact path='/crypto/:coindId' element={<CryptoDetails/>}/>
-                <Route exact path='/stockrecommendations' element={<StockRecommendations/>}/>
-              </Routes>
+      <div className='Navbar'>
+        <Navbar />
+      </div>
+      <div className='Main'>
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route exact path='/crypto/:coindId' element={<CryptoDetails />} />
+              <Route exact path='/stockrecommendations' element={<StockRecommendations />} />
+            </Routes>
 
-            </div>
-          </Layout>
+          </div>
+        </Layout>
 
-        </div>
-        <div className='Footer'>
+      
+      <div className='Footer'>
+        <Typography.Title level={4} style={{ color: 'black', textAlign: 'center' }}>
+          Cryptotracker <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/" style={{color:'black'}}>Home</Link>
+          <Link to="/stockrecommendations" style={{color: 'black'}}>Stock Recommendations</Link>
+          <Link to="/cryptocurrencies" style={{color: 'black'}}>Cryptocurrencies</Link> 
+        </Space>
 
-        </div>
-       
+      </div>
+      </div>
+
     </div>
   )
 }
--- cryptotracker/src/index.js
+++ cryptotracker/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import './style.css';
 import App from './App';
-//import 'antd/dist/antd.css' när vi väl har css-filen
+import 'antd/dist/antd.css' // behövs egentligen en css-fil
 
 ReactDOM.render(
     <Router>
